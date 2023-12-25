9ab5de71d0ff6836376bd45780d3601a7c5df12a
Owen <technicatalyst78@gmail.com>
2023-12-25 13:30:52 +0100

    Initial Commit

---
+++ MET/METApp.swift
@@ -0,0 +1,17 @@
+//
+//  METApp.swift
+//  MET
+//
+//  Created by Adedola Owen Abaru on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct METApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
